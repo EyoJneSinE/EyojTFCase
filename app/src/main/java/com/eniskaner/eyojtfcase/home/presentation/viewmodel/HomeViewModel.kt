15c1f9849e56f750e18668b5757f4839339f8c21
Enis Kaner <enis.d.kaner@gmail.com>
2024-09-05 23:58:10 +0300

    add: HomeViewModel for get detekt count from Firebase fire store

---
+++ app/src/main/java/com/eniskaner/eyojtfcase/home/presentation/viewmodel/HomeViewModel.kt
@@ -0,0 +1,32 @@
+package com.eniskaner.eyojtfcase.home.presentation.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.eniskaner.eyojtfcase.common.util.HomeUIState
+import com.eniskaner.eyojtfcase.home.data.repo.HomeRepository
+import com.google.firebase.auth.FirebaseAuth
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val homeRepository: HomeRepository
+): ViewModel() {
+
+    private var firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
+    private val _sugarBeetCountState = MutableLiveData<HomeUIState<Int>>()
+    val sugarBeetCountState: LiveData<HomeUIState<Int>>
+        get() = _sugarBeetCountState
+
+    init {
+        getSugarBeetCountFromFireStore()
+    }
+
+    fun getSugarBeetCountFromFireStore() {
+        _sugarBeetCountState.value = HomeUIState.Loading
+        homeRepository.getSugarBeetCount {
+            _sugarBeetCountState.value = it
+        }
+    }
+}
